5c0843014cf67db046b9d59f4c44ca45e3af9977
Christophe Cauet <ccauet@point-8.de>
2018-06-20 08:40:13 +0200

    Changed repo structure, fixes #1

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ import os
 from setuptools import setup
 
 setup(
-    name="pipedrive-backup",
-    description="CLI script to backup a Pipedrive CRM instance",
-    url="https://git.point-8.de/",
+    name="pipetools",
+    description="CLI script to interact with a Pipedrive CRM instance",
+    url="https://github.com/point8/pipetools",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3 :: Only",
     ],
     version="1",
+    packages=['pipetools'],
     entry_points={"console_scripts": ["pipetools = pipetools:main"]},
     install_requires=["tqdm", "pendulum"],
 )

688cd525647f645a4709b8c0813eb8ebb84b3165
Jesko Merkel <jmerkel@point-8.de>
2019-12-06 14:17:10 +0100

    Adds cmd to fetch a single deal

--- pipetools/__init__.py
+++ pipetools/__init__.py
@@ -26,7 +26,7 @@ def mkdir(path):
         os.makedirs(path)
 
 
-def get(base_url, token, outdir=".", path="users", limit=100, stdout=False, ids=[], suppress_w2hdd=False):
+def get(base_url, token, outdir=".", path="users", limit=100, stdout=False, ids=[]):
     collected_ids = []
    
     if len(ids)==0:
@@ -85,23 +85,8 @@ def get(base_url, token, outdir=".", path="users", limit=100, stdout=False, ids=
     if stdout:
         print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
     else:
-        if suppress_w2hdd==False:
-            with open(os.path.join(outdir, f"{path}.json"), "w") as out_file:
-                json.dump(data, out_file, indent=4, sort_keys=True)
-    return data
-
-def project_proposal_informations_from_json(data):
-    d=data.pop()
-    print('\\newcommand{\\cnamelong}{'+d['org_id']['name']+'\\xspace}')
-    print('\\newcommand{\\caddress}{'+d['org_id']['address']+'}')
-    print('\\newcommand{\\ccontactname}{'+ d['person_id']['name']+'}')
-    print('\\newcommand{\\ccontactdetails}{Tel. '+d['person_id']['phone'][0]['value']+'\\\\'+d['person_id']['email'][0]['value']+'}')    
-    #for i in d['person_id']['email']
-    #    print(i['value'])
-    #for i in d['person_id']['phone']:
-    #    print(i['value'])
-
-
+        with open(os.path.join(outdir, f"{path}.json"), "w") as out_file:
+            json.dump(data, out_file, indent=4, sort_keys=True)
 
 
 @click.group(help="Command line tools for Pipedrive CRM")
@@ -139,14 +124,17 @@ def backup(outdir, token, topic, stdout):
 
 
 
-@cli.command("proposal",  help="Dump information from specific deal for a proposal")
-@click.option("--token",  help="Pipedrive API token", prompt=True)
-@click.option("--deal",   help="Select deal id (e.g. 64)", prompt=True)
-def proposal(token, deal):
-    data = get(BASE_URL, token, path="deals", limit=1, stdout=False, ids=[deal] if not isinstance(deal, list) else deal, suppress_w2hdd=True)
-    project_proposal_informations_from_json(data)
+@cli.command("deal",      help="Fetch specific deal as JSON")
+@click.argument("outdir", default=".")
+@click.option("--token",  help="Pipedrive CRE API token", prompt=True)
+@click.option("--dealid", help="Select deal with id (e.g. 64)", prompt=True)
+@click.option("--stdout", help="Output to stdout instead of file", is_flag=True)
+def deal(outdir, token, dealid, stdout):
+    if not stdout:
+        print(f"Saving JSON deal data to {outdir}")
+    mkdir(outdir)
+    data = get(BASE_URL, token, path="deals", limit=1, stdout=stdout, ids=[dealid] if not isinstance(dealid, list) else dealid)
 
 def main():
     cli()
-
 
